578fc00dbcb4d244d7cfd46adecf457e18983e93
Avi98 <9958703925.dad@gmail.com>
2018-08-27 10:51:43 -0700

    feat(register): register page setup

--- src/components/onBoarding/Register.js
+++ src/components/onBoarding/Register.js
@@ -1,9 +1,77 @@
 import React, { Component } from "react";
-
+import OnboardingSkelton from "./OnboardingSkeleton";
 class Register extends Component {
   state = {};
   render() {
-    return <div>Register</div>;
+    return (
+      <OnboardingSkelton
+        topHeader={"Do what you love"}
+        subHeader={"Register for free"}
+        showDownloadStore
+      >
+        {({ onSubmitLogin }) => (
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="User name"
+              />
+              <img src="images/checked.svg" />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Email"
+              />
+              <img src="images/error.svg" />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+              />
+              <img src="images/checked.svg" />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Repeat Password"
+              />
+              <img src="images/error.svg" />
+            </div>
+            <div className="form-group">
+              <ul className="options">
+                <li>
+                  <input
+                    type="radio"
+                    id="male"
+                    name="amount"
+                    className="black_button"
+                    checked
+                  />
+                  <label for="male">Male</label>
+                </li>
+                <li>
+                  <input type="radio" id="female" name="amount" />
+                  <label for="female">Female</label>
+                </li>
+              </ul>
+            </div>
+            <div className="form-group">
+              <button className="blue_button">Register</button>
+            </div>
+          </form>
+        )}
+      </OnboardingSkelton>
+    );
   }
 }
 
