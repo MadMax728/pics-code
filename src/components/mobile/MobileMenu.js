fd4478aee53e041e8413ff26eb119c8b6b4a1766
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-09-04 19:02:17 +0530

    fixed pushed issue

--- src/components/mobile/MobileMenu.js
+++ src/components/mobile/MobileMenu.js
@@ -57,6 +57,7 @@ export default class MobileMenu extends Component {
           <div className="row">
             <div
               role="button"
+              tabIndex={0}
               htmlFor="backButton"
               className="back_option padding-15"
               onClick={this.handleOnSetSidebarClose}
