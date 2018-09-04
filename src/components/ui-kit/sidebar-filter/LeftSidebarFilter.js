89b14db1ade2fb5649d82a71a1632d09b64afa20
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-09-04 18:23:15 +0530

    fixed pushed issue

--- src/components/ui-kit/sidebar-filter/LeftSidebarFilter.js
+++ src/components/ui-kit/sidebar-filter/LeftSidebarFilter.js
@@ -13,10 +13,10 @@ const LeftSidebarFilter = ({ filters }) => {
           <div className="filter-wrapper" key={filter.name}>
             <div className={filter.className}>{filter.name}</div>
             {filter.type === "radio" &&
-              filter.items.map(({ item, index }) => {
+              filter.items.map(item => {
                 return (
                   <Radio
-                    key={index}
+                    key={item.name}
                     forUse={filter.name}
                     name={item.name}
                     className={item.className}
