5745da1fc3bd2c2139f77726e3074d4fa14c3b36
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-09-05 19:13:46 +0530

    fixed the leftsidebar issue

---
+++ src/components/ui-kit/sidebar-filter/radioBtn/RadioBtn.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import propTypes from "prop-types";
+import { RadioGroup, Radio } from "react-radio-group";
+
+class RadioBtn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedValue: "",
+      name: "",
+      data: {}
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      selectedValue: this.props.items[0].name,
+      name: this.props.name
+    });
+  }
+
+  handleChange = value => {
+    this.setState({ selectedValue: value });
+    const { data } = this.state;
+    data.values = { name: this.state.name, val: value };
+    this.props.onChange(data);
+  };
+
+  render() {
+    const { items, name, foruse, className } = this.props;
+
+    return (
+      //eslint-disable-next-line jsx-a11y/no-onchange
+
+      <RadioGroup
+        name={name}
+        selectedValue={this.state.selectedValue}
+        onChange={this.handleChange}
+      >
+        {items.map(item => {
+          return (
+            <label className={item.className} key={item.name}>
+              <span>{item.name}</span>
+              <Radio value={item.name} />
+              <span className="checkmark" />
+            </label>
+          );
+        })}
+      </RadioGroup>
+    );
+  }
+}
+
+Radio.propTypes = {
+  items: propTypes.any,
+  name: propTypes.string,
+  foruse: propTypes.string,
+  onChange: propTypes.func,
+  className: propTypes.string
+};
+
+export default RadioBtn;
