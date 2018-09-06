f5cb82f5fdf25e518a6517bca1fa0b5eb657e6c7
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-09-06 14:06:42 +0530

    added lib folder

---
+++ src/lib/constants/routes.js
@@ -0,0 +1,44 @@
+export const ROOT_ROUTE = "/";
+export const LOGIN_ROUTE = "/login";
+export const REGISTER_ROUTE = "/register";
+export const MY_PROFILE_ROUTE = "/profile";
+export const NEWS_ROUTE = "/news";
+export const EXPLORE_ROUTE = "/expore";
+export const PARTICIPANTS_ROUTE = "/participants";
+export const USER_ROUTE = "/user";
+export const PICS_ROUTE = "/pics";
+export const NOTIFICATIONS_ROUTE = "/notifications";
+export const MESSAGES_ROUTE = "/home/messages";
+export const FORGOT_PASSWORD = "/forgotPassword";
+export const CAMPAIGN_ROUTE = "/campaign";
+export const RESET_EMAIL = "/resetEmail";
+export const RESET_PASSWORD = "/resetPassword/:token";
+
+// Mobile view Side Menu
+
+// Information
+export const ABOUTUS_ROUTE = "/aboutus";
+export const LEGAL_NOTICE_ROUTE = "/legal_notice";
+export const SUPPORT_ROUTE = "/support";
+export const CAMPAIGNS_ROUTE = "/campaigns";
+export const ADS_ROUTE = "/ads";
+export const VERIFICATION_ROUTE = "/verification";
+
+//Services
+export const TERMS_CONDITIONS_ROUTE = "/terms_conditions";
+export const DATA_PROTECTION_AND_PRIVACY_POLICY_ROUTE =
+  "/data_protection_and_privacy_policy";
+export const TEARMS_USE_ROUTE = "/terms_use";
+export const PLATFORM_POLICY_ROUTE = "/paltform_policy";
+export const ADVERTISING_POLICY_ROUTE = "/advertising_policy";
+export const CAMPAIGN_POLICY_ROUTE = "/campaign_policy";
+export const NETZDG_ROUTE = "/netzDg";
+export const REPORTED_CONTENT_ROUTE = "/reported_content";
+export const PAYMENT_METHOD_ROUTE = "/payment_method";
+export const CANCELLATION_POLICY_ROUTE = "/cancellation_policy";
+export const DATA_DOWNLOAD_ROUTE = "/data_download";
+export const DELETE_ACCOUNT_ROUTE = "/delete_account";
+export const LAW_ENFORCEMENT_AGENCY_ROUTE = "/law_enforcement_agency";
+export const REPORT_PROBLEM_ROUTE = "/report_problem";
+export const COOKIES_ROUTE = "/cookies";
+export const FEEDBACK_ROUTE = "/feedback";
