f61e794652278c6bb44fed0b08b15ecc2bc8ee0b
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-09-24 20:05:39 +0530

    implemented upload modal basic and fixed lint issues

---
+++ src/components/web/user/upload/Upload.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Upload = () => {
+  console.log("ahi ave che");
+
+  return (
+    <div className="middle-section padding-rl-10">
+      <div className="about-wrapper">
+        <div className="general-info">
+          <div className="section-title">Upload information </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Upload;
