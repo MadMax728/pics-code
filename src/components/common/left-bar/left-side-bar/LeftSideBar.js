1ec92ea3019ed968a0c1ceb60fb5b3bd901010c9
santosh <webwerks@santosh.local>
2018-09-28 22:29:40 +0530

    fixed save routes

--- src/components/common/left-bar/left-side-bar/LeftSideBar.js
+++ src/components/common/left-bar/left-side-bar/LeftSideBar.js
@@ -42,7 +42,6 @@ class LeftSideBar extends Component {
         <Route path={routes.CAMPAIGN_ROUTE} exact component={CampaignNav} />
         <Route path={routes.CREATOR_ROUTE} exact component={CampaignNav} />
         <Route path={routes.COMPANY_ROUTE} exact component={CampaignNav} />
-        <Route path={routes.MY_PROFILE_ROUTE} exact component={DashboardNav} />
         <Route
           path={routes.NOTIFICATIONS_ROUTE}
           exact
@@ -130,7 +129,11 @@ class LeftSideBar extends Component {
         {/* ...... Owner Menu ...... */}
 
         {/* ...... Other Menu ...... */}
-
+        <Route
+          path={routes.MY_PROFILE_ROUTE}
+          exact
+          component={SideBarOtherMenu}
+        />
         <Route
           path={routes.OTHER_NEWS_FEED_ROUTE}
           exact
