3204ccaad2f10ad4bffb2e2017516955bee4c40f
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-10-01 19:57:36 +0530

    add files for the mobile view component and implemented the payment component attached for the campaign and ads

---
+++ src/components/web/stand-alone/about-us-information/AboutUsInformation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutUsInformation = () => {
+  return (
+    <div>
+      <div className="normal_title padding-15">AboutUsInformation</div>
+    </div>
+  );
+};
+
+export default AboutUsInformation;
---
+++ src/components/web/stand-alone/campaign-information/CampaignInformation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CampaignInformation = () => {
+  return (
+    <div>
+      <div className="normal_title padding-15">CampaignInformation</div>
+    </div>
+  );
+};
+
+export default CampaignInformation;
