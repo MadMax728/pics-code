7cc60a1ea4eb86e9a89832c580a50c637169be16
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-10-15 18:43:27 +0530

    add files for the mockup data for the dashboard and user module

---
+++ src/mock-data/notification/like-you/likeYou.js
@@ -0,0 +1,53 @@
+import * as images from "../../../lib/constants/images";
+
+export const likeYou_list = [
+  {
+    isMessage: false,
+    isLikeToo: true,
+    username: "sagar",
+    date: "01.01.2000",
+    image: images.pic_1
+  },
+  {
+    isMessage: true,
+    isLikeToo: false,
+    username: "vaghela",
+    date: "01.01.2000",
+    image: images.pic_2
+  },
+  {
+    isMessage: false,
+    isLikeToo: true,
+    username: "santoh",
+    date: "01.01.2000",
+    image: images.pic_2
+  },
+  {
+    isMessage: true,
+    isLikeToo: false,
+    username: "shinde",
+    date: "01.01.2000",
+    image: images.pic_1
+  },
+  {
+    isMessage: true,
+    isLikeToo: false,
+    username: "amit",
+    date: "01.01.2000",
+    image: images.pic_2
+  },
+  {
+    isMessage: false,
+    isLikeToo: true,
+    username: "parmar",
+    date: "01.01.2000",
+    image: images.pic_1
+  },
+  {
+    isMessage: true,
+    isLikeToo: false,
+    username: "sachin",
+    date: "01.01.2000",
+    image: images.pic_2
+  }
+];
---
+++ src/mock-data/notification/request/request.js
@@ -0,0 +1,39 @@
+import * as images from "../../../lib/constants/images";
+
+export const request_list = [
+  {
+    username: "sagar",
+    date: "01.01.2000",
+    image: images.pic_1
+  },
+  {
+    username: "vaghela",
+    date: "01.01.2000",
+    image: images.pic_2
+  },
+  {
+    username: "santoh",
+    date: "01.01.2000",
+    image: images.pic_2
+  },
+  {
+    username: "shinde",
+    date: "01.01.2000",
+    image: images.pic_1
+  },
+  {
+    username: "amit",
+    date: "01.01.2000",
+    image: images.pic_2
+  },
+  {
+    username: "parmar",
+    date: "01.01.2000",
+    image: images.pic_1
+  },
+  {
+    username: "sachin",
+    date: "01.01.2000",
+    image: images.pic_2
+  }
+];
