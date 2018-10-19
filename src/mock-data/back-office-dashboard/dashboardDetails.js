1a3d51501e3a999408af2eb0fb9e7ea25c274f53
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-10-19 15:53:43 +0530

    add mock data file for the back office dashboard

---
+++ src/mock-data/back-office-dashboard/dashboardDetails.js
@@ -0,0 +1,907 @@
+export const key_statistics = [
+  {
+    registered_users: "100",
+    active_users: "90",
+    deactivated_accounts: "10",
+    turnover: "10000",
+    reviews: "85",
+    reported_content: "50"
+  }
+];
+
+export const content_statistics = [
+  {
+    lable: "Total",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "All",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "No category",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Art & Culture",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Sports & Health",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Travel",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Beauty & Cosmetics",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Fashion",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Technology",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Food & Drink",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Animals",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Automobiles",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Business",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Craftmen",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Entertainment",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Outdoor",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Education",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Charity",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Science",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Gaming",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Home & Living",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Natur",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Spirituality",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "News & Politics",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Music",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Movies & Photography",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Family",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Company",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  },
+  {
+    lable: "Other",
+    images: "",
+    videos: "",
+    campaign_company: "",
+    image_campaign: "",
+    video_campaign: "",
+    campaign_creator: "",
+    pics: "",
+    ads: "",
+    total: ""
+  }
+];
+
+export const campaign_statistics_company = [
+  {
+    lable: "Total",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Art & Culture",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Sports & Health",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Travel",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Beauty & Cosmetics",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Fashion",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Technology",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Food & Drink",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Animals",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Automobiles",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Business",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Craftmen",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Entertainment",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Outdoor",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Education",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Charity",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Science",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Gaming",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Home & Living",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Nature",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Spirituality",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "News & Politics",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Music",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Film & Photography",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Family",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Company",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Other",
+    number: "",
+    active_campaign: "",
+    closed_campaign: "",
+    campaigns_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  }
+];
+
+export const ads_statisitcs = [
+  {
+    lable: "Total",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Art & Culture",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Sports & Health",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Travel",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Beauty & Cosmetics",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Fashion",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Technology",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Food & Drink",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Animals",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Automobiles",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Business",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Craftmen",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Entertainment",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Outdoor",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Education",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Charity",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Science",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Gaming",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Home & Living",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Nature",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Spirituality",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "News & Politics",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Music",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Film & Photography",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Family",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Company",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  },
+  {
+    lable: "Other",
+    number: "",
+    active_ads: "",
+    closed_ads: "",
+    ads_temp_closed: "",
+    potential_turnover: "",
+    total_turnover: "",
+    average_conv_rate: ""
+  }
+];
---
+++ src/mock-data/back-office-dashboard/index.js
@@ -0,0 +1,6 @@
+export {
+  key_statistics,
+  content_statistics,
+  campaign_statistics_company,
+  ads_statisitcs
+} from "./dashboardDetails";
