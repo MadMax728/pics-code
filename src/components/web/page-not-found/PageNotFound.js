2954c297a5172924395b3d34b99df2a1e790144c
Surandase <komal.surandase@neosofttech.com>
2018-10-24 12:08:06 +0530

    error fix

--- src/components/web/page-not-found/PageNotFound.js
+++ src/components/web/page-not-found/PageNotFound.js
@@ -10,8 +10,8 @@ class PageNotFound extends Component {
             <div className="row">
               <div className="heading">This page isn't available</div>
               <div className="sub-heading">
-                The link you followed may be broken, or the page may have been
-                removed
+                {`The link you followed may be broken, or the page may have been
+                removed`}
               </div>
               <div className="logo-background" />
             </div>
