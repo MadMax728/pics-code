3a3a508386f14643b476593fb440090def8f5879
Surandase <komal.surandase@neosofttech.com>
2018-11-21 11:16:42 +0530

    resolved issue

--- src/components/web/templates/PicturesPage.js
+++ src/components/web/templates/PicturesPage.js
@@ -34,20 +34,20 @@ class PicturesRoot extends React.Component {
         {isLoading && (
           <div className="col-xs-12 no-padding">
             <div className="col-sm-6 pic-block-wrapr pic-left-block padding-lr-5">
-              <div class="pic-block gray_content">
+              <div className="pic-block gray_content">
                 <img src={images.placeholder_pic} alt="profile_pic" />
-                <div class="name-wrapper gray_box">
-                  <div class="username" />
-                  <div class="name" />
+                <div className="name-wrapper gray_box">
+                  <div className="username" />
+                  <div className="name" />
                 </div>
               </div>
             </div>
             <div className="col-sm-6 pic-block-wrapr pic-right-block padding-lr-5">
-              <div class="pic-block gray_content">
+              <div className="pic-block gray_content">
                 <img src={images.placeholder_pic} alt="profile_pic" />
-                <div class="name-wrapper gray_box">
-                  <div class="username" />
-                  <div class="name" />
+                <div className="name-wrapper gray_box">
+                  <div className="username" />
+                  <div className="name" />
                 </div>
               </div>
             </div>
