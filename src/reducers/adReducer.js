2d37ab14c1ec5d9ffd3b505b7a7e67d44ce8790b
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-11-21 16:22:20 +0530

    add files for ad reducer and actions for ad module

---
+++ src/reducers/adReducer.js
@@ -0,0 +1,50 @@
+import * as types from "../lib/constants/actionTypes";
+import initialState from "./initialState";
+
+const adReducer = (state = initialState.adData, action) => {
+  switch (action.type) {
+    // Get Ads
+    case types.GET_ADS_STARTED:
+      return {
+        ...state,
+        isLoading: true,
+        error: null
+      };
+    case types.GET_ADS_SUCCEEDED:
+      return {
+        ...state,
+        ads: action.payload,
+        isLoading: false
+      };
+    case types.GET_ADS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    // Ad Details
+    case types.GET_AD_DETAILS_STARTED:
+      return {
+        ...state,
+        isLoading: true,
+        error: null
+      };
+    case types.GET_AD_DETAILS_SUCCEEDED:
+      return {
+        ...state,
+        ad: action.payload,
+        isLoading: false
+      };
+    case types.GET_AD_DETAILS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default adReducer;
