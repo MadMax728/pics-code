2d6399f4efd3f0f7383eec190db0620b32f0e174
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2018-11-21 11:50:51 +0530

    implmented userpic dummy component for loading

--- src/components/ui-kit/loading-indicator/index.js
+++ src/components/ui-kit/loading-indicator/index.js
@@ -1,2 +1,3 @@
 export { default as InlineLoading } from "./InlineLoading";
 export { default as CampaignLoading } from "./CampaignLoading";
+export { default as UserPicLoading } from "./UserPicLoading";
