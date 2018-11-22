4521f6832a5765332fa67654221f8b21088fae69
Surandase <komal.surandase@neosofttech.com>
2018-11-22 16:48:50 +0530

    worked on misc static pages

--- src/components/web/information/verification/Verification.js
+++ src/components/web/information/verification/Verification.js
@@ -69,14 +69,14 @@ class Verification extends Component {
                 </div>
                 <div className="row">
                   <div className="col-sm-12">
-                    <div className="form-group capcha-wrapper">
+                    <div className="form-group capcha-wrapper col-sm-6">
                       <img
                         src={images.capcha}
                         alt={"capcha"}
                         className="capcha-img"
                       />
                     </div>
-                    <div className="form-group button_wrapper">
+                    <div className="form-group button_wrapper col-sm-6">
                       <button className="black_button wid120">Send</button>
                     </div>
                   </div>
