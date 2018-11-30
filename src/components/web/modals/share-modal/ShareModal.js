2e57d45d3df40eff19aab4ef9ccc7225317900b0
Surandase <komal.surandase@neosofttech.com>
2018-11-30 16:56:04 +0530

    fixed issue

--- src/components/web/modals/share-modal/ShareModal.js
+++ src/components/web/modals/share-modal/ShareModal.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { CustomBootstrapModal } from "../../../ui-kit";
 import propTypes from "prop-types";
 import { Share } from "../../user";
+import { Link } from "react-router-dom";
+import * as images from "../../../../lib/constants/images";
+import * as routes from "../../../../lib/constants/routes";
 
 class ShareModal extends Component {
   constructor(props, context) {
@@ -12,15 +15,37 @@ class ShareModal extends Component {
   render() {
     const { handleModalInfoHide, modalInfoShow } = this.props;
     return (
-      <CustomBootstrapModal
-        modalClassName={"modal fade share-popup"}
-        header={false}
-        footer={false}
-        modalShow={modalInfoShow}
-        closeBtn={false}
-        handleModalHide={handleModalInfoHide}
-        modalBodyContent={<Share handleModalInfoHide={handleModalInfoHide} />}
-      />
+      // <CustomBootstrapModal
+      //   modalClassName={"modal fade share-popup"}
+      //   header={false}
+      //   footer={false}
+      //   modalShow={modalInfoShow}
+      //   closeBtn={false}
+      //   handleModalHide={handleModalInfoHide}
+      //   modalBodyContent={<Share handleModalInfoHide={handleModalInfoHide} />}
+      // />
+      <ul className="share-side-popup">
+        <li className="share-side-popup-item">
+          <Link to={routes.SETTINGS_EDIT_PROFILE_ROUTE}>
+            <span className="fa fa-facebook"></span>
+           </Link>
+        </li>
+        <li className="share-side-popup-item">
+          <Link to={routes.SETTINGS_EDIT_PROFILE_ROUTE}>
+            <span className="fa fa-instagram"></span>
+           </Link>
+        </li>
+        <li className="share-side-popup-item">
+          <Link to={routes.SETTINGS_EDIT_PROFILE_ROUTE}>
+            <span className="fa fa-youtube"></span>
+           </Link>
+        </li>
+        <li className="share-side-popup-item">
+          <Link to={routes.SETTINGS_EDIT_PROFILE_ROUTE}>
+            <span className="fa fa-twitter"></span>
+           </Link>
+        </li>
+      </ul>
     );
   }
 }
