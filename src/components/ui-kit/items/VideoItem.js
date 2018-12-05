51a989d7d6959bcac9221955fb00e238778216a3
santosh shinde <santosh.shinde@wwindia.com>
2018-12-05 13:43:11 +0530

    add video player

--- src/components/ui-kit/items/VideoItem.js
+++ src/components/ui-kit/items/VideoItem.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import ReactPlayer from "react-player";
+import VideoPlayer from "../player/VideoPlayer";
 
 class VideoItem extends Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      videoJsOptions : {
+        autoplay: false,
+        controls: true,
+        muted: true,
+        crossOrigin: true,
+        sources: [{
+          src: this.props.item,
+          type: 'video/mp4'
+        }]
+      }
+    }
+    
   }
   render() {
+    const { videoJsOptions } = this.state;
     return (
       <div className="feed_image">
         <div className={"embed-responsive embed-responsive-16by9"}>
           <div className={"img-responsive embed-responsive-item"}>
-            <ReactPlayer url={this.props.item} playing={false} controls/>
+            {/* <ReactPlayer url={this.props.item} playing={false} controls/> */}
+            <VideoPlayer { ...videoJsOptions } />
           </div>
         </div>
       </div>
