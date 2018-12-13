112ab7107516bde1a3892a18af2bfd59d587a35c
Priyanka Aher <priyanka.aher@neosofttech.com>
2018-12-13 14:06:53 +0530

    Data download form validation - changes added

--- src/components/web/templates/settings/DataDownloadPage.js
+++ src/components/web/templates/settings/DataDownloadPage.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Text } from "../../../ui-kit/CommonUIComponents";
+import { Auth } from "../../../../auth";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getDownloadData } from "../../../../actions";
+import * as images from "../../../../lib/constants/images";
 
 class DataDownloadPage extends Component {
   constructor(props) {
@@ -9,19 +14,54 @@ class DataDownloadPage extends Component {
       form: {
         email: "",
         password: ""
-      }
+      },
+      error: {}
     };
   }
+
+  formValid = () => {
+    const errors = {};
+    let isFormValid = true;
+    const { form } = this.state;
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (form.email.length === 0) {
+      errors.email = "Email is required.";
+      isFormValid = false;
+    }
+    const isValidemail = emailRegex.test(form.email);
+    if (!isValidemail) {
+      isFormValid = false;
+      errors.email = "Email ID should be valid.";
+    }
+    if (form.password.length === 0) {
+      errors.password = "Password is required.";
+      isFormValid = false;
+    }
+    this.setState({ error: errors });
+    return isFormValid;
+  };
 
   handleChangeField = event => {
     const { form } = this.state;
     form[event.values.name] = event.values.val;
     this.setState({ form });
+    this.formValid();
   };
 
   // handelSubmit called when click on submit
   handleSubmit = e => {
     e.preventDefault();
+    if (!this.formValid()) {
+      return false;
+    }
+    const { form } = this.state;
+    const data = {
+      email: form.email,
+      password: form.password
+    };
+    this.props.getDownloadData(data).then(() => {
+      // To Do - On Download Data
+    });
   };
 
   render() {
@@ -57,21 +97,41 @@ class DataDownloadPage extends Component {
                     type="text"
                     className="form-control"
                     id="email"
+                    placeholder="Email"
                     name="email"
+                    value={form.email ? form.email : ""}
                     onChange={this.handleChangeField}
                   />
+                  {form.password.length === 0 ? (
+                    <img src={images.error} alt={"error"} />
+                  ) : (
+                    <img src={images.checked} alt={"checked"} />
+                  )}
+                  <span className="error-msg highlight">
+                    {this.state.error.email}
+                  </span>
                 </div>
               </div>
               <div className="col-sm-5 padding-l-5 padding-r-5">
                 <div className="form-group">
                   <label htmlFor="password">Password</label>
                   <Text
-                    type="text"
+                    type="password"
                     className="form-control"
                     id="password"
                     name="password"
+                    placeholder="Password"
+                    value={form.password ? form.password : ""}
                     onChange={this.handleChangeField}
                   />
+                  {form.password.length === 0 ? (
+                    <img src={images.error} alt={"error"} />
+                  ) : (
+                    <img src={images.checked} alt={"checked"} />
+                  )}
+                  <span className="error-msg highlight">
+                    {this.state.error.password}
+                  </span>
                 </div>
               </div>
               <div className="col-sm-2 padding-l-5 btn-wrapper">
@@ -94,4 +154,19 @@ class DataDownloadPage extends Component {
   }
 }
 
-export default DataDownloadPage;
+const mapStateToProps = state => ({
+  downloadData: state.downloadData
+});
+
+const mapDispatchToProps = {
+  getDownloadData
+};
+
+DataDownloadPage.propTypes = {
+  getDownloadData: PropTypes.func
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DataDownloadPage);
