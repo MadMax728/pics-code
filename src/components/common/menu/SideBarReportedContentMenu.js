77b03b51e1deb44d1f8c0fba5a2e110ab733214b
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2019-01-04 18:58:00 +0530

    rename file name

---
+++ src/components/common/menu/SideBarReportedContentMenu.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { LeftSidebarNav } from "../../ui-kit";
+import * as routes from "../../../lib/constants/routes";
+import { Translations } from "../../../lib/translations";
+
+const Links = [
+  {
+    to: routes.BACK_OFFICE_REPORTED_IMAGES_ROUTE,
+    className: "secondary_title",
+    activeClassName: "active",
+    text: Translations.review_content_menu.images
+  },
+  {
+    to: routes.BACK_OFFICE_REPORTED_VIDEOS_ROUTE,
+    className: "secondary_title",
+    activeClassName: "active",
+    text: Translations.review_content_menu.videos
+  },
+  {
+    to: routes.BACK_OFFICE_REPORTED_CAMPAIGNS_ROUTE,
+    className: "secondary_title",
+    activeClassName: "active",
+    text: Translations.review_content_menu.campaigns
+  },
+  {
+    to: routes.BACK_OFFICE_REPORTED_PICS_ROUTE,
+    className: "secondary_title",
+    activeClassName: "active",
+    text: Translations.review_content_menu.pics
+  },
+  {
+    to: routes.BACK_OFFICE_REPORTED_ADS_ROUTE,
+    className: "secondary_title",
+    activeClassName: "active",
+    text: Translations.review_content_menu.ads
+  },
+  {
+    to: routes.BACK_OFFICE_REPORTED_COMMENTS_ROUTE,
+    className: "secondary_title",
+    activeClassName: "active",
+    text: Translations.review_content_menu.comments
+  },
+  {
+    to: routes.BACK_OFFICE_REPORTED_USER_ROUTE,
+    className: "secondary_title",
+    activeClassName: "active",
+    text: Translations.review_content_menu.user
+  }
+];
+
+const SideBarReportedContentMenu = () => {
+  return (
+    <LeftSidebarNav
+      links={Links}
+      header={`Reported Content`}
+      ulClassName={"nav navbar-nav pull-right settings-menu"}
+    />
+  );
+};
+
+export default SideBarReportedContentMenu;
