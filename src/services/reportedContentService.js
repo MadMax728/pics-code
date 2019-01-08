b3f48c4ac75c227aff878d528bdfa5d7e8de21b2
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2019-01-08 18:35:13 +0530

    add files for Redux

---
+++ src/services/reportedContentService.js
@@ -0,0 +1,62 @@
+import { api } from "../api";
+
+// Developers can override this with an env.local file
+const baseUrl = process.env.REACT_APP_API_BASEURL;
+
+/**
+ *
+ * @param {*} payload
+ */
+
+export const getReportedContent = (payload, type="get", header = {}) => {
+  let apiURL;
+  switch (type) {
+    case "get":
+      apiURL = `/reports?reportContent=${payload.reportContent}`;
+      break;
+    case "search":
+      apiURL = `/reports?reportContent=${payload.reportContent}&searchType=${payload.searchType}`;
+      break;
+    default: 
+      apiURL = "/reports?reportContent=Image";
+      break;
+  }
+  return api(baseUrl, header).get(apiURL);
+};
+
+
+// Update BackOffice Report
+export const updateBackOfficeReport = (payload, header = {}) =>
+  api(baseUrl, header).put("/reports", payload );
+
+// Add Report
+export const addReport = (payload, header = {}) =>
+  api(baseUrl, header).post("/reports", payload );
+
+// Get Reported Statistics
+export const getReportedStatistics = (payload, header = {}) =>
+  api(baseUrl, header).get("/reports/post-count?reportContent=" + payload.reportContent);
+
+// BackOffice VideoBO API
+export const reportedContentVideos = (payload, header = {}) =>
+  api(baseUrl, header).get("/newsfeeds/news-feeds");
+
+// BackOffice Campaign API
+export const reportedContentCampaigns = (payload, header = {}) =>
+  api(baseUrl, header).get("/newsfeeds/news-feeds");
+
+// BackOffice Pics API
+export const reportedContentPics = (payload, header = {}) =>
+  api(baseUrl, header).get("/newsfeeds/news-feeds");
+
+// BackOffice Ads API
+export const reportedContentAds = (payload, header = {}) =>
+  api(baseUrl, header).get("/newsfeeds/news-feeds");
+
+// BackOffice Comments API
+export const reportedContentComments = (payload, header = {}) =>
+  api(baseUrl, header).get("/comments");
+
+// BackOffice User API
+export const reportedContentUsers = (payload, header = {}) =>
+  api(baseUrl, header).get("/users/get-all-users");
