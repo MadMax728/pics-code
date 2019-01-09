fe4e4afdf2b76fbc95720cc01308155a3ce4b719
Sagar <sagar.jagtap@wwindia.com>
2019-01-09 18:27:06 +0530

    added scroll event

--- src/components/web/user/messages/MRightActiveChat.js
+++ src/components/web/user/messages/MRightActiveChat.js
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Translations } from "../../../../lib/translations";
 import MessagesLazyList from './MessagesLazyList';
 
-const MRightActiveChat = (
-    { 
-        user,
-        socket
-    }) => {
-        return (
-            <div className={"active-chat"}>
-                {
-                    user && user.id ? (
-                        <MessagesLazyList user={user} socket={socket} />
-                    ) : (
-                        <div className="full-width text-center">
-                             <h6>{Translations.messages_modal.ready}</h6>
-                        </div>
-                    )
-                }
-            </div>
-        )
-};
+class MRightActiveChat extends Component { 
+    
+    constructor(){
+        super();
+        this.trackScrolling = this.trackScrolling.bind(this);
+    }
 
+    trackScrolling = () => {
+        const wrappedElement = document.getElementById("header-chat-"+this.props.user.id);
+        // console.log(wrappedElement.scrollTop ,wrappedElement.scrollLeft);
+        if(wrappedElement)
+        console.log(wrappedElement.scrollTop ,wrappedElement.scrollLeft);
+        if (wrappedElement && (wrappedElement.scrollTop === wrappedElement.scrollLeft)) {
+            console.log('header bottom reached');
+        }
+    };
+
+    render() {
+        const { socket, user } = this.props;
+        return (
+            <div onScroll={this.trackScrolling} className={"active-chat"} id={"header-chat-"+user.id}>
+            {
+                user && user.id ? (
+                    <MessagesLazyList user={user} socket={socket} />
+                ) : (
+                    <div className="full-width text-center">
+                         <h6>{Translations.messages_modal.ready}</h6>
+                    </div>
+                )
+            }
+        </div>
+        )
+    }
+} 
 
 MRightActiveChat.propTypes = {
     user: PropTypes.any,
