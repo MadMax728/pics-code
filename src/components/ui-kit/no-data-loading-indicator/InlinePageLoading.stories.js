1c62160acea8af973931136bc160d5469560ce43
Priyanka Aher <priyanka.aher@neosofttech.com>
2019-01-11 12:07:26 +0530

    RightSidebar Loading page - added

--- src/components/ui-kit/no-data-loading-indicator/InlinePageLoading.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import InlinePageLoading from "./InlinePageLoading";
-
-storiesOf("Loading Indicators", module).add("LeftSidebarNav", () => {
-  return <InlinePageLoading message={""} />;
-});
