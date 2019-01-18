613b49276977630f987ca28d9ff1fde43ed2b616
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2019-01-18 14:20:27 +0530

    implementation of the campaign information card

--- src/components/web/templates/information/InformationPage.js
+++ src/components/web/templates/information/InformationPage.js
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
-import Comments from "./Comments";
-
-import * as images from "../../../../lib/constants/images";
-import { Translations } from "../../../../lib/translations";
-
-import { modalType } from "../../../../lib/constants/enumerations";
-import { RenderToolTips } from "../../../common";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
+
 import {
   getCampaignDetails,
   like,
@@ -15,11 +10,14 @@ import {
   setSavedPost,
   addReport
 } from "../../../../actions";
+
+import { Translations } from "../../../../lib/translations";
+import { modalType } from "../../../../lib/constants/enumerations";
+
+import { RenderToolTips } from "../../../common";
 import { getBackendPostType } from "../../../Factory";
-import { connect } from "react-redux";
-import { ThreeDots, CampaignDetailsLoading } from "../../../ui-kit";
-import moment from "moment";
-import * as routes from "../../../../lib/constants/routes";
+import { CampaignDetailsLoading } from "../../../ui-kit";
+import CampaignDetailsCard from "../../../misc/CampaignDetailsCard";
 
 class InformationPage extends Component {
   constructor(props, context) {
@@ -48,6 +46,7 @@ class InformationPage extends Component {
   componentDidMount = () => {
     window.scrollTo(0, 0);
     this.getCampaignDetailsData();
+    this.handleCommentsSections();
   };
 
   getCampaignDetailsData = () => {
@@ -199,236 +198,17 @@ class InformationPage extends Component {
     return (
       <div className="padding-l-10 middle-section width-80">
         {campaignDetails && !isLoading && (
-          <div className="information-wrapper ht100">
-            <div className="info-inner-wrapper col-xs-12">
-              <div className="info-main-title paddindLeft0">
-                {campaignDetails.title}
-              </div>
-              <div className="text">{campaignDetails.description}</div>
-              <img src={campaignDetails.profileImage} alt={"information"} />
-              <div className="text paddTop20">
-                {campaignDetails.description}
-              </div>
-              {campaignDetails.isOwner ||
-              campaignDetails.isAlreadyParticipant ? (
-                ""
-              ) : (
-                <button
-                  className="filled_button"
-                  onClick={this.handleApplyParticipant}
-                  id={campaignDetails.id}
-                >
-                  {Translations.apply_campaign}
-                </button>
-              )}
-              <div className="feed_wrapper">
-                <div className="feed_header">
-                  <div className="no-padding profile_image">
-                    <img
-                      src={images.image}
-                      alt="circle-img-1"
-                      className="img-circle img-responsive"
-                    />
-                  </div>
-                  <div className="col-sm-9 col-xs-7">
-                    <div className="normal_title">{campaignDetails.title}</div>
-                    <div className="secondary_title">
-                      {campaignDetails.userName}
-                    </div>
-                    <div className="grey_title">{campaignDetails.category}</div>
-                  </div>
-                  <div className="col-sm-2 col-xs-2 like_wrapper">
-                    {campaignDetails.isSelfLike ? (
-                      <img
-                        src={images.blue_heart}
-                        alt="like-1"
-                        className="pull-right"
-                        role="presentation"
-                        onClick={this.handleFavorite}
-                        id={campaignDetails.id}
-                        onKeyDown={this.handleOnKeyDown}
-                      />
-                    ) : (
-                      <img
-                        src={images.feed_like}
-                        alt="like"
-                        className="pull-right"
-                        role="presentation"
-                        onClick={this.handleFavorite}
-                        id={campaignDetails.id}
-                        onKeyDown={this.handleOnKeyDown}
-                      />
-                    )}
-                  </div>
-                </div>
-                <div className="feed_content col-xs-12">
-                  <div className="feed_description col-xs-12">
-                    <div className="col-sm-6 no-padding">
-                      <div className="info_wrapper">
-                        <span className="normal_title">Start: </span>
-                        <span className="secondary_title">
-                          {moment(campaignDetails.startDate).format(
-                            "MMMM Do YYYY"
-                          )}
-                        </span>
-                      </div>
-                      <div className="info_wrapper">
-                        <span className="normal_title">Procedure: </span>
-                        <span className="secondary_title">
-                          {campaignDetails.procedure}
-                        </span>
-                      </div>
-                      <div className="info_wrapper">
-                        <span className="normal_title">Target group: </span>
-                        <span className="secondary_title">
-                          {campaignDetails.target_group}
-                        </span>
-                      </div>
-                    </div>
-                    <div className="col-sm-6 no-padding">
-                      <div className="info_wrapper">
-                        <span className="normal_title">End: </span>
-                        <span className="secondary_title">
-                          {moment(campaignDetails.endDate).format(
-                            "MMMM Do YYYY"
-                          )}
-                        </span>
-                      </div>
-                      <div className="info_wrapper">
-                        <span className="normal_title">Type: </span>
-                        <span className="secondary_title">
-                          {campaignDetails.typeContent}
-                        </span>
-                      </div>
-                      <div className="info_wrapper">
-                        <span className="normal_title">Applications: </span>
-                        <span className="secondary_title">
-                          {campaignDetails.applications
-                            ? campaignDetails.applications
-                            : "22/22"}
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="feed_description col-xs-12">
-                    <div className="col-sm-6 no-padding">
-                      <div className="info_wrapper">
-                        <span className="normal_title">
-                          {Translations.create_campaigns.offer}:{" "}
-                        </span>
-                        <span className="secondary_title">
-                          {campaignDetails.offerTagList &&
-                            campaignDetails.offerTagList[0].offerTagName}
-                        </span>
-                      </div>
-                      <div className="info_wrapper">
-                        <span className="normal_title">
-                          {Translations.create_campaigns.inquiry}:{" "}
-                        </span>
-                        <span className="secondary_title">
-                          {campaignDetails.inquiryTagList &&
-                            campaignDetails.inquiryTagList[0].inquiryTagName}
-                        </span>
-                      </div>
-                    </div>
-                    <div className="col-sm-6 no-padding">
-                      <div className="info_wrapper">
-                        <span className="normal_title">
-                          {Translations.create_campaigns.offer_tag}:{" "}
-                        </span>
-                        <span className="secondary_title">
-                          {campaignDetails.offerTagList &&
-                            campaignDetails.offerTagList[0].offerTagName}
-                        </span>
-                      </div>
-                      <div className="info_wrapper">
-                        <span className="normal_title">
-                          {Translations.create_campaigns.inquiry_tag}:{" "}
-                        </span>
-                        <span className="secondary_title">
-                          {campaignDetails.inquiryTagList &&
-                            campaignDetails.inquiryTagList[0].inquiryTagName}
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="feed_footer margin-t-15 margin-b-15 padding-lr-30">
-                  <div className="messages">
-                    <span className="count">
-                      {campaignDetails.commentCount}
-                    </span>
-                    <img
-                      src={images.comment}
-                      alt={"feed_msg"}
-                      role="presentation"
-                      id={campaignDetails.createdBy}
-                      onKeyDown={this.handleOnKeyDown}
-                      onClick={this.handleCommentsSections}
-                    />
-                  </div>
-                  <div className="likes">
-                    <span className="count">{campaignDetails.likeCount}</span>
-                    {campaignDetails.isSelfLike ? (
-                      <img
-                        src={images.blue_heart}
-                        alt="like-1"
-                        className="pull-right"
-                        role="presentation"
-                        onClick={this.handleFavorite}
-                        id={campaignDetails.id}
-                        onKeyDown={this.handleOnKeyDown}
-                      />
-                    ) : (
-                      <img
-                        src={images.feed_like}
-                        alt="like"
-                        className="pull-right"
-                        role="presentation"
-                        onClick={this.handleFavorite}
-                        id={campaignDetails.id}
-                        onKeyDown={this.handleOnKeyDown}
-                      />
-                    )}
-                  </div>
-                  <div className="show_more_options">
-                    <ThreeDots
-                      id="report"
-                      role="button"
-                      dataTip="tooltip"
-                      dataClass="tooltip-wrapr"
-                      getContent={() => this.renderReportTips()}
-                      effect="solid"
-                      delayHide={500}
-                      delayShow={500}
-                      delayUpdate={500}
-                      place={"left"}
-                      border
-                      type={"light"}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="feed_wrapper">
-              <div className="feed-comment">
-                {/* Comments Section */}
-                {isComments && (
-                  <Comments
-                    campaign={campaignDetails}
-                    campaignComments={comments}
-                    campeignId={campaignDetails.id}
-                    typeContent={campaignDetails.typeContent}
-                    handleComment={this.handleComment}
-                    totalCommentsCount={comments.length}
-                    isReport={false}
-                  />
-                )}
-              </div>
-            </div>
-          </div>
+          <CampaignDetailsCard 
+            campaignDetails={campaignDetails} 
+            isComments={isComments} 
+            comments={comments} 
+            handleApplyParticipant={this.handleApplyParticipant} 
+            handleCommentsSections={this.handleCommentsSections}
+            handleFavorite={this.handleFavorite}
+            handleOnKeyDown={this.handleOnKeyDown}
+            renderReportTips={this.renderReportTips}
+            handleComment={this.handleComment}
+          />
         )}
         {isLoading && <CampaignDetailsLoading count={1} />}
       </div>
