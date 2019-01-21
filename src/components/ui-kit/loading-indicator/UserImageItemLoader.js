a7ea3c81e5339e3733271ba4a68e062e152430c6
Santosh Shinde <santosh.shinde@wwindia.com>
2019-01-21 12:32:37 +0530

    fixed the html string and items comp

---
+++ src/components/ui-kit/loading-indicator/UserImageItemLoader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserImageItemLoader  = () => {
+    return (
+        <div className="no-padding profile_image wid45">
+            <span className="gray_box pro-image"></span>
+        </div>
+  );
+}
+
+export default UserImageItemLoader;
