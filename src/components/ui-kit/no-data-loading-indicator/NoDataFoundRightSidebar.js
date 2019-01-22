b1de528e4779cc4c2eed8dc1232da83149e2ffd8
komal <komal.surandase@neosofttech.com>
2019-01-22 17:45:21 +0530

    no record found

--- src/components/ui-kit/no-data-loading-indicator/NoDataFoundRightSidebar.js
+++ src/components/ui-kit/no-data-loading-indicator/NoDataFoundRightSidebar.js
@@ -1,27 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 import * as images from "../../../lib/constants/images";
-const NoDataFoundRightSidebar = () => {
+
+const NoDataFoundRightSidebar = ( {
+    image
+}) => {
     return (
         <div className="feed_wrapper" >
             <div className="datanotfound-wrapper" >
-            <img src={images.no_community_pic} alt="no_data_image" className="img-responsive"/>
-                {/* <div className="notfound-body"> 
-                    <div className="notfound-title col-sm-8 col-xs-8">
-                        <p>No Data Found</p>
-                    </div>
-                    <div className="notfound-btn-wrapper col-sm-2 col-xs-2">
-                        <button className="data-btns" id="1" ><i className="fa fa-plus-square" aria-hidden="true"></i></button>
-                    </div>
-                    <div className="notfound-btn-wrapper col-sm-2 col-xs-2">
-                        <button className="data-btns" id="2" ><i className="fa fa-refresh" aria-hidden="true"></i></button>
-                    </div>
-                </div> */}
-                <div className="notfound-footer">
-                    <div className="col-md-12"><span>Write an artical</span>on LinkedIn</div>
-                </div>
+                <img src={image} alt="no_data_image" className="img-responsive"/>
             </div>
         </div>
     );
 }
+
+NoDataFoundRightSidebar.propTypes = {
+    image: PropTypes.string
+};
+
+
+NoDataFoundRightSidebar.defaultProps = {
+    image: `${images.no_community_pic}`
+};
 
 export default NoDataFoundRightSidebar;
