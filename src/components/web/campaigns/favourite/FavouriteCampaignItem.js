2711984f36bd90f8998374a5253dd11e2f14f8f5
Sagar Vaghela <sagar.vaghela@neosofttech.com>
2019-02-08 16:53:03 +0530

    implemented date in favourite campaign list

--- src/components/web/campaigns/favourite/FavouriteCampaignItem.js
+++ src/components/web/campaigns/favourite/FavouriteCampaignItem.js
@@ -3,11 +3,15 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import * as routes from "../../../../lib/constants/routes";
 import * as images from "../../../../lib/constants/images";
+import { DateFormat } from "../../../Factory";
+import { Translations } from "../../../../lib/translations";
 
 const FavouriteCampaignItem = ({ campaign }) => {
   // const profile_route = campaign.isOwner
   //   ? routes.NEWS_FEED_ROUTE
   //   : `${routes.NEWS_FEED_ROUTE}/${campaign.userName}`;
+  console.log(campaign);
+  
   const profile_image = campaign.profileImage? campaign.profileImage : images.image;
   return (
     <Link to={`${routes.BASE_CAMPAIGN_INFORMATION_ROUTE}${campaign.id}`}>
@@ -23,7 +27,7 @@ const FavouriteCampaignItem = ({ campaign }) => {
           <div className="normal_title">{campaign.title}</div>
           <div className="secondary_title">{campaign.userName}</div>
           <div className="grey_title">
-            {campaign.category && campaign.category[0].categoryName}
+            {campaign.startDate && DateFormat(campaign.startDate, Translations.date_format.time, true)}
           </div>
         </div>
       </div>
