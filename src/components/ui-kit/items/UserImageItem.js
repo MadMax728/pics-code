aa0ff76adc11fb2a628b1c69a626cdb61c40a5cc
Santosh Shinde <santosh.shinde@wwindia.com>
2019-02-08 13:51:27 +0530

    add user image item for participants

--- src/components/ui-kit/items/UserImageItem.js
+++ src/components/ui-kit/items/UserImageItem.js
@@ -7,7 +7,7 @@ import { UserImageItemLoader } from '../loading-indicator';
 class UserImageItem extends PureComponent {
 
   render() {
-    const { item, customClass } = this.props;
+    const { item, customClass, isParticipant, campaignUserProfile } = this.props;
     return (
         <div className={`profile_image ${customClass}`}>
              <LazyLoad height={45} once offset={[-45, 0]} placeholder={<UserImageItemLoader />}>
@@ -15,14 +15,19 @@ class UserImageItem extends PureComponent {
                 src={item ? item : images.image}
                 alt="profile"
                 className="img-circle img-responsive"
-                />
-                <span className="overap-pro-img">
-                  <img
-                  src={item ? item : images.image}
-                  alt="profile"
-                  className="img-circle img-responsive"
-                  />
-                </span>
+                /> 
+                {
+                  isParticipant && (
+                    <span className="overap-pro-img">
+                        <img
+                        src={campaignUserProfile ? campaignUserProfile : images.image}
+                        alt="profile"
+                        className="img-circle img-responsive"
+                        />
+                    </span>
+                  )
+                }
+               
             </LazyLoad>
         </div>
     );
@@ -31,9 +36,15 @@ class UserImageItem extends PureComponent {
 
 UserImageItem.propTypes = {
   item: PropTypes.string.isRequired,
+  isParticipant: PropTypes.bool,
+  campaignUserProfile: PropTypes.string,
   customClass: PropTypes.string,
   isLoading: PropTypes.bool,
   // isOtherCardExist: PropTypes.bool.isRequired
+};
+
+UserImageItem.defaultProps = {
+  isParticipant: false
 };
 
 export default UserImageItem;
