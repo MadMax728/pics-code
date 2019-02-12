26bce49c502b23f0968d057abe3e983bd9a88c81
Priyanka Aher <priyanka.aher@neosofttech.com>
2019-02-12 11:40:24 +0530

    Campaign details changed as per user type

--- src/components/misc/InformationCard.js
+++ src/components/misc/InformationCard.js
@@ -4,8 +4,10 @@ import moment from "moment";
 import { Link } from "react-router-dom";
 import { Translations } from "../../lib/translations";
 import * as routes from "../../lib/constants/routes";
+import { DateFormat } from "../Factory";
 
 const InformationCard = ({ item, type }) => {
+  const selectedUserType = "creator";
   return (
     <Link to={`${routes.BASE_CAMPAIGN_INFORMATION_ROUTE}${item.id}`}>
       <div className="feed_description padding-10">
@@ -18,50 +20,62 @@ const InformationCard = ({ item, type }) => {
         <div className="col-sm-6 no-padding">
           <div className="info_wrapper">
             <span className="normal_title">
-              {Translations.create_campaigns.start}:{" "}
+              {Translations.campaign_details.start}:{" "}
             </span>
             <span className="secondary_title">
               {moment.unix(item.startDate).format("MMMM Do YYYY")}
             </span>
           </div>
-          <div className="info_wrapper">
-            <span className="normal_title">
-              {Translations.create_campaigns.procedure}:{" "}
-            </span>
-            <span className="secondary_title">{item.procedure}</span>
-          </div>
-          <div className="info_wrapper">
-            <span className="normal_title">
-              {Translations.create_campaigns.target_group}:{" "}
-            </span>
-            <span className="secondary_title">
-              {item.target_group ? item.target_group : "Male"}
-            </span>
-          </div>
+
+          {item && item.userType !== selectedUserType && (
+            <div className="info_wrapper">
+              <span className="normal_title">
+                {Translations.campaign_details.procedure}:{" "}
+              </span>
+              <span className="secondary_title">{item.procedure}</span>
+            </div>
+          )}
+
+          {item && item.userType !== selectedUserType && (
+            <div className="info_wrapper">
+              <span className="normal_title">
+                {Translations.campaign_details.applications}:{" "}
+              </span>
+              <span className="secondary_title">
+                {item.applicationCount ? item.applicationCount : "0"}
+              </span>
+            </div>
+          )}
         </div>
         <div className="col-sm-6 no-padding">
           <div className="info_wrapper">
             <span className="normal_title">
-              {Translations.create_campaigns.end}:{" "}
+              {Translations.campaign_details.end}:{" "}
             </span>
             <span className="secondary_title">
               {moment.unix(item.endDate).format("MMMM Do YYYY")}
             </span>
           </div>
-          <div className="info_wrapper">
-            <span className="normal_title">
-              {Translations.create_campaigns.type}:{" "}
-            </span>
-            <span className="secondary_title">{item.typeContent}</span>
-          </div>
-          <div className="info_wrapper">
-            <span className="normal_title">
-              {Translations.create_campaigns.applications}:{" "}
-            </span>
-            <span className="secondary_title">
-              {item.applications ? item.applications : "0/0"}
-            </span>
-          </div>
+
+          {item && item.userType !== selectedUserType && (
+            <div className="info_wrapper">
+              <span className="normal_title">
+                {Translations.campaign_details.type}:{" "}
+              </span>
+              <span className="secondary_title">{item.typeContent}</span>
+            </div>
+          )}
+
+          {item && item.target_group && (
+            <div className="info_wrapper">
+              <span className="normal_title">
+                {Translations.campaign_details.target_group}:{" "}
+              </span>
+              <span className="secondary_title">
+                {item.target_group ? item.target_group : "Male"}
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </Link>
