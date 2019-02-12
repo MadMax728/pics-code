5e4d719b670dd5bd79e9d205c0640bae87ade714
Priyanka Aher <priyanka.aher@neosofttech.com>
2019-02-12 09:37:43 +0530

    about Page changes added

--- src/components/misc/body/AboutCardBody.js
+++ src/components/misc/body/AboutCardBody.js
@@ -14,6 +14,7 @@ const AboutCardBody = ({ about }) => {
     userInfo = JSON.parse(storage.userInfo);
   }
   const selectedUserType = "creator";
+  console.log(about);
   return (
     <div>
       <div className="about-wrapper">
@@ -26,99 +27,136 @@ const AboutCardBody = ({ about }) => {
               <span>{Translations.about.username}</span>
               <span>{about.username}</span>
             </li>
-            <li>
-              <span>
-                {userInfo && userInfo.userType === selectedUserType
-                  ? Translations.about.user
-                  : Translations.about.name_company}
-              </span>
-              <span>{about.name}</span>
-            </li>
-            {userInfo && userInfo.userType === selectedUserType ? (
+            {about && about.name && (
               <li>
-                <span>{Translations.about.date_of_birth}</span>
                 <span>
-                  {moment
-                    .unix(about.birthDate)
-                    .format(Translations.date_format.date)}
+                  {userInfo && userInfo.userType === selectedUserType
+                    ? Translations.about.user
+                    : Translations.about.name_company}
                 </span>
+                <span>{about.name}</span>
               </li>
-            ) : (
-              ""
             )}
-            {userInfo && userInfo.userType === selectedUserType ? (
+
+            {about &&
+              about.birthDate &&
+              (userInfo && userInfo.userType === selectedUserType ? (
+                <li>
+                  <span>{Translations.about.date_of_birth}</span>
+                  <span>
+                    {moment
+                      .unix(about.birthDate)
+                      .format(Translations.date_format.date)}
+                  </span>
+                </li>
+              ) : (
+                ""
+              ))}
+
+            {about &&
+              about.gender &&
+              (userInfo && userInfo.userType === selectedUserType ? (
+                <li>
+                  <span>{Translations.about.gender}</span>
+                  <span>{about.gender}</span>
+                </li>
+              ) : (
+                ""
+              ))}
+
+            {about && about.category && about.category[0].categoryName ? (
               <li>
-                <span>{Translations.about.gender}</span>
-                <span>{about.gender}</span>
+                <span>{Translations.about.category}</span>
+                <span>{about.category && about.category[0].categoryName}</span>
               </li>
             ) : (
               ""
             )}
-            <li>
-              <span>{Translations.about.category}</span>
-              <span>{about.category && about.category[0].categoryName}</span>
-            </li>
-            <li>
-              <span>{Translations.about.location}</span>
-              <span>{about.location && about.location.address}</span>
-            </li>
-            <li>
-              <span>{Translations.about.phone_number}</span>
-              <span>{about.phone_number}</span>
-            </li>
-            <li>
-              <span>{Translations.about.email}</span>
-              <span>{about.email}</span>
-            </li>
-            <li>
-              <span>{Translations.about.web_site}</span>
-              <span>{about.website}</span>
-            </li>
+
+            {about && about.location && about.location.address && (
+              <li>
+                <span>{Translations.about.location}</span>
+                <span>{about.location && about.location.address}</span>
+              </li>
+            )}
+
+            {about && about.phone_number && (
+              <li>
+                <span>{Translations.about.phone_number}</span>
+                <span>{about.phone_number}</span>
+              </li>
+            )}
+
+            {about && about.email && (
+              <li>
+                <span>{Translations.about.email}</span>
+                <span>{about.email}</span>
+              </li>
+            )}
+
+            {about && about.website && (
+              <li>
+                <span>{Translations.about.web_site}</span>
+                <span>{about.website}</span>
+              </li>
+            )}
           </ul>
         </div>
 
-        <div className="profile-description">
-          <div className="section-title">
-            {Translations.about.profile_description}
+        {about && about.profileDescription && (
+          <div className="profile-description">
+            <div className="section-title">
+              {Translations.about.profile_description}
+            </div>
+            <div className="profile-content">{about.profileDescription}</div>
           </div>
-          <div className="profile-content">{about.profileDescription}</div>
-        </div>
+        )}
 
-        <div className="personal-interest">
-          <div className="section-title">
-            {Translations.about.personal_interest}
-          </div>
-          <div className="section-subtitle">{Translations.about.offer_tag}</div>
-          <div className="tags-wrapper">
-            {about &&
-              about.offerTagList &&
-              about.offerTagList.length &&
-              about.offerTagList.map(offer => {
-                return (
-                  <Link to={""} key={offer.id}>
-                    {offer.offerTagName}
-                  </Link>
-                );
-              })}
-          </div>
-        </div>
-        <div className="inquiry-tag">
-          <div className="section-subtitle">
-            {Translations.about.inquiry_tag}
-          </div>
-          <div className="tags-wrapper">
-            {about &&
-              about.inquiryTagList.length !== undefined &&
-              about.inquiryTagList.map(inquiry => {
-                return (
-                  <Link to={""} key={inquiry.id}>
-                    {inquiry.inquiryTagName}
-                  </Link>
-                );
-              })}
-          </div>
-        </div>
+        {about &&
+          about.offerTagList &&
+          about.offerTagList.length &&
+          about.inquiryTagList.length !== undefined && (
+            <div>
+              <div className="personal-interest">
+                <div className="section-title">
+                  {Translations.about.personal_interest}
+                </div>
+                <div className="section-subtitle">
+                  {Translations.about.offer_tag}
+                </div>
+                <div className="tags-wrapper">
+                  {about &&
+                    about.offerTagList &&
+                    about.offerTagList.length &&
+                    about.offerTagList.map(offer => {
+                      return (
+                        <Link to={""} key={offer.id}>
+                          {offer.offerTagName}
+                        </Link>
+                      );
+                    })}
+                </div>
+              </div>
+              <div className="inquiry-tag">
+                <div className="section-subtitle">
+                  {Translations.about.inquiry_tag}
+                </div>
+                <div className="tags-wrapper">
+                  {about &&
+                    about.inquiryTagList.length !== undefined &&
+                    about.inquiryTagList.map(inquiry => {
+                      return (
+                        <Link to={""} key={inquiry.id}>
+                          {inquiry.inquiryTagName}
+                        </Link>
+                      );
+                    })}
+                </div>
+              </div>
+            </div>
+          )}
       </div>
+
       <div className="about-social-wrapr col-xs-12 no-padding">
         <SocialNetworks userId={"123"} isOwnerProfile={false} />
       </div>
