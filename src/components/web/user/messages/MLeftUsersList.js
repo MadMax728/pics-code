f388b832f9885bfbc592700e60f5bfba13e03b33
Priyanka Aher <priyanka.aher@neosofttech.com>
2019-02-20 10:30:43 +0530

    Language change using state changes added

--- src/components/web/user/messages/MLeftUsersList.js
+++ src/components/web/user/messages/MLeftUsersList.js
@@ -1,49 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
-import MLeftUserItem from './MLeftUserItem';
+import MLeftUserItem from "./MLeftUserItem";
+import { Translations } from "../../../../lib/translations";
 
-const MLeftUsersList = (
-    { 
-        items,
-        handleChatClick
-    }) => {
-        const latestUserList = () => {
-            return items.map((item, key) => (
-                <div
-                    role="button"
-                    tabIndex={key}
-                    className={classnames("chat-wrapper",{ "new" : !item.read })}
-                    key={item.id}
-                    data-id={item.id}
-                    data-value={item.username}
-                    onClick={handleChatClick}
-                    onKeyDown={handleChatClick}
-                  >
-                    <MLeftUserItem key={key} item={item} message={item.lastMessage} />
-                </div>
-            ));
-        };
-        return (
-            <div className="user-chat-wrapper">
-                {
-                    items && items.length ? (
-                        latestUserList()
-                    ) : (
-                        <div className="card">
-                                No message yet...
-                        </div>
-                    )
-                }
-            </div>
-        )
-};
+class MLeftUsersList extends Component {
+  render() {
+    const { items, handleChatClick } = this.props;
 
+    const latestUserList = () => {
+      return items.map((item, key) => (
+        <div
+          role="button"
+          tabIndex={key}
+          className={classnames("chat-wrapper", {
+            new: !item.read
+          })}
+          key={item.id}
+          data-id={item.id}
+          data-value={item.username}
+          onClick={handleChatClick}
+          onKeyDown={handleChatClick}
+          ref={el => {
+            this.messagesEnd = el;
+          }}
+        >
+          <MLeftUserItem key={key} item={item} message={item.lastMessage} />
+        </div>
+      ));
+    };
+    return (
+      <div className="user-chat-wrapper">
+        {items && items.length ? (
+          latestUserList()
+        ) : (
+          <div className="card">{Translations.message_module.no_msg}</div>
+        )}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    let username = "marc1";
+    if (username === "marc1") {
+      this.scrollBottomOnNewMessage(username);
+    }
+  }
+
+  scrollBottomOnNewMessage = username => {
+    // console.log(username);
+    // let data = "user_" + username;
+    // console.log(this.data);
+    console.log(this.messagesEnd);
+    if (this.messagesEnd) {
+      this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+}
 
 MLeftUsersList.propTypes = {
-    handleChatClick: PropTypes.func,
-    items: PropTypes.any
+  handleChatClick: PropTypes.func,
+  items: PropTypes.any
 };
-  
+
 export default MLeftUsersList;
-  
