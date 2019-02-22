2264ad92588dc2a15f191613b589c6282b832e7e
Santosh Shinde <santosh.shinde@wwindia.com>
2019-02-22 14:38:32 +0530

    fixed build changes

--- log-dashboard/src/config.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const getUrl = `http://picstagraph-backend-dev2.us-east-1.elasticbeanstalk.com/api/`;
-   
-export const postUrl = `http://picstagraph-backend-dev2.us-east-1.elasticbeanstalk.com/api/auth/login`;
-
-  
--- plopfile.js
+++
@@ -1,5 +0,0 @@
-// Plop code generation - see https://plopjs.com/documentation/
-// eslint-disable-next-line import/no-commonjs
-module.exports = function(plop) {
-  plop.load(["./packages/plop-pack-picstagraph-react"]);
-};
