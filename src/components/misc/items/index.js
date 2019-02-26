571ee3a300790fc2a774b1c6deb60238f9c7ea30
Priyanka Aher <priyanka.aher@neosofttech.com>
2019-02-26 16:31:51 +0530

    Comment section changes added

--- src/components/misc/items/index.js
+++ src/components/misc/items/index.js
@@ -1,3 +1,3 @@
 export { default as InfoWrapperItem } from "./InfoWrapperItem";
 export { default as DescriptionItem } from "./DescriptionItem";
-//export { default as ReportTips } from "./ReportTips";
+export { default as ReportTips } from "./ReportTips";
