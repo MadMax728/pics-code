a3b8b04133784f2547572b231e7fb470cf61e145
Santosh Shinde <santosh.shinde@wwindia.com>
2019-03-01 15:26:35 +0530

    fixed lint

--- src/components/web/search/SearchUsers.js
+++ src/components/web/search/SearchUsers.js
@@ -1,31 +1,33 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Scrollbars } from "react-custom-scrollbars";
+import * as Infinite from "react-infinite"
 import SearchUserItem from "./SearchUserItem";
 
-const SearchUsers = ( { users }) => {
+const SearchUsers = ( { users = [], onInfiniteLoad }) => {
     return (
         <div className="search-user">
-            <Scrollbars
-                style={{}}
-                autoHeight
-                autoHeightMin={0}
-                autoHeightMax={400}>
-                {
-                    users.map((user, key) => (
-                        <div className="search-user-wrapper" key={user._id}>
-                            <SearchUserItem item={user}></SearchUserItem>
-                        </div>
-                    ))
-                } 
-            </Scrollbars>
+            {
+                users && users.length > 0 && (
+                    <Infinite containerHeight={400} infiniteLoadBeginEdgeOffset={300} elementHeight={40} onInfiniteLoad={onInfiniteLoad}>
+                        {
+                            users.map((user, key) => (
+                                <div className="search-user-wrapper" key={user._id}>
+                                    <SearchUserItem item={user}></SearchUserItem>
+                                </div>
+                            ))
+                        } 
+                    </Infinite>
+                )
+            }
         </div>
     );
 };
 
 
 SearchUsers.propTypes = {
-    users: PropTypes.any
+    users: PropTypes.any,
+    onInfiniteLoad: PropTypes.any
 };
   
 export default SearchUsers;
