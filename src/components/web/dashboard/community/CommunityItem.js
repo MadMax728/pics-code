9fb6c651a7609761a184a0c754f9f4efcb8324fd
Santosh Shinde <santosh.shinde@wwindia.com>
2019-03-04 12:20:08 +0530

    fixed community

--- src/components/web/dashboard/community/CommunityItem.js
+++ src/components/web/dashboard/community/CommunityItem.js
@@ -1,31 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { Translations } from "../../../../lib/translations";
 import * as routes from "../../../../lib/constants/routes";
 import { UserImageItem, Button, UserTitleItem } from "../../../ui-kit";
 
-const CommunityItem = ({ user, handleActionClick, isLoading = false, classNames='', btnText= '' }) => {
+const CommunityItem = ({ user, handleSubscribe, handleUnSubscribe, isLoading = false}) => {
   return (
     <div className="community_wrapper">
-      <div className="community-user-image">
-        <Link to={`${routes.ABOUT_ROUTE}/${user.username}`}>
-            <UserImageItem 
-              customClass={'img-circle img-responsive'} 
-              item={user.profileUrl}>
-            </UserImageItem>
-        </Link>
-      </div>
-      <div className="community-user-name">
-        <UserTitleItem title={user.name} username={user.username}></UserTitleItem>
-      </div>
-      <div className="community-subscribe">
-        <Button
-          className={classNames}
-          id={user.id}
-          onClick={handleActionClick}
-          disabled={isLoading}
-          text={btnText}
-        />
+      <Link to={`${routes.ABOUT_ROUTE}/${user.username}`}>
+        <div className="community-user-image">
+          <UserImageItem 
+            customClass={'img-circle img-responsive'} 
+            item={user.profileUrl}>
+          </UserImageItem>
+        </div>
+        <div className="community-user-name">
+          <UserTitleItem 
+            title={user.name} 
+            username={user.username}>
+          </UserTitleItem>
+        </div>
+      </Link>
+      <div className="community-subscribe"> 
+        {
+          user.isSubscribed ? (
+            <Button
+              className={`filled_button`}
+              id={user.subscribedId}
+              onClick={handleUnSubscribe}
+              disabled={isLoading}
+              text={Translations.profile_community_right_sidebar.Subscribed}
+          />
+          ) : (
+            <Button
+              className={`blue_button`}
+              id={user.id}
+              onClick={handleSubscribe}
+              disabled={isLoading}
+              text={Translations.profile_community_right_sidebar.Subscribe}
+          />
+          )
+        }
       </div>
     </div>
   );
@@ -33,10 +49,9 @@ const CommunityItem = ({ user, handleActionClick, isLoading = false, classNames=
 
 CommunityItem.propTypes = {
   user: PropTypes.any,
-  handleActionClick: PropTypes.any,
+  handleSubscribe: PropTypes.any,
+  handleUnSubscribe: PropTypes.any,
   isLoading: PropTypes.any,
-  classNames: PropTypes.any,
-  btnText: PropTypes.any
 };
 
 export default CommunityItem;
