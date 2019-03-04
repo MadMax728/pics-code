c85acad307c6d75aaba98bc15966e1dc5dfe7055
Santosh Shinde <santosh.shinde@wwindia.com>
2019-03-04 12:22:30 +0530

    fixed community

--- src/components/web/dashboard/community/CommunityItem.js
+++ src/components/web/dashboard/community/CommunityItem.js
@@ -8,20 +8,22 @@ import { UserImageItem, Button, UserTitleItem } from "../../../ui-kit";
 const CommunityItem = ({ user, handleSubscribe, handleUnSubscribe, isLoading = false}) => {
   return (
     <div className="community_wrapper">
-      <Link to={`${routes.ABOUT_ROUTE}/${user.username}`}>
-        <div className="community-user-image">
-          <UserImageItem 
-            customClass={'img-circle img-responsive'} 
-            item={user.profileUrl}>
-          </UserImageItem>
-        </div>
-        <div className="community-user-name">
+      <div className="community-user-image">
+        <Link to={`${routes.ABOUT_ROUTE}/${user.username}`}>
+            <UserImageItem 
+              customClass={'img-circle img-responsive'} 
+              item={user.profileUrl}>
+            </UserImageItem>
+        </Link>
+      </div>
+      <div className="community-user-name">
+        <Link to={`${routes.ABOUT_ROUTE}/${user.username}`}>
           <UserTitleItem 
             title={user.name} 
             username={user.username}>
           </UserTitleItem>
-        </div>
-      </Link>
+        </Link>
+      </div>
       <div className="community-subscribe"> 
         {
           user.isSubscribed ? (
